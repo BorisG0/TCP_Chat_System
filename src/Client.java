a9d3e578a4f4f23bc8913b15c92ca4b523e5f806
DEV-AdrianC <catinella.adrian@gmail.com>
2023-03-30 16:56:30 +0200

    Client mit 2 Threads

--- src/Client.java
+++ src/Client.java
@@ -3,26 +3,42 @@ import java.net.*;
 
 public class Client {
     public static void main(String[] args) {
-        BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
-        Socket socket;
-        BufferedReader networkIn;
-        PrintWriter networkOut;
+        // Verbindung zum Server herstellen
+        try {
+            Socket socket = new Socket("localhost", Server.DEFAULT_PORT);
 
-        while(true){
-            try{
-                String userLine = userIn.readLine(); //auf Tastatureingabe warten
+            // Thread 1: Tastatureingabe lesen und an Server senden
+            Thread sendThread = new Thread(() -> {
+                try {
+                    BufferedReader networkIn  = new BufferedReader(new InputStreamReader(System.in));
+                    PrintWriter networkOut = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    String userLine;
+                    while ((userLine = networkIn.readLine()) != null) {
+                        networkOut.println(userLine);
+                        networkOut.flush();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            sendThread.start();
 
-                socket = new Socket("localhost", Server.DEFAULT_PORT); //Verbindung zum Server
-                networkIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                networkOut = new PrintWriter(socket.getOutputStream());
+            // Thread 2: Antwort des Servers lesen und auf Konsole ausgeben
+            Thread receiveThread = new Thread(() -> {
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println("Received message from server: " + line);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            receiveThread.start();
 
-                networkOut.println(userLine); //Befehl an Server schicken
-                networkOut.flush();
-
-                System.out.println(networkIn.readLine()); //Antwort vom Server anzeigen
-            }catch (Exception e){
-                e.printStackTrace();
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
