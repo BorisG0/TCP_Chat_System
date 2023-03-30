ffb98ae9524a9d9704529b51898ddba8cd2786aa
Boris Gratchev <boris@gratchev.com>
2023-03-30 14:33:05 +0200

    client server connection

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,28 @@
+import java.io.*;
+import java.net.*;
+
 public class Client {
+    public static void main(String[] args) {
+        BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
+        Socket socket;
+        BufferedReader networkIn;
+        PrintWriter networkOut;
+
+        while(true){
+            try{
+                String userLine = userIn.readLine(); //auf Tastatureingabe warten
+
+                socket = new Socket("localhost", Server.DEFAULT_PORT); //Verbindung zum Server
+                networkIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                networkOut = new PrintWriter(socket.getOutputStream());
+
+                networkOut.println(userLine); //Befehl an Server schicken
+                networkOut.flush();
+
+                System.out.println(networkIn.readLine()); //Antwort vom Server anzeigen
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,23 @@
-import java.net.ServerSocket;
+import java.io.*;
+import java.net.*;
 
 public class Server {
     public static final int DEFAULT_PORT = 7777;
     public static void main(String[] args) {
         try{
             ServerSocket server = new ServerSocket(DEFAULT_PORT);
+            Socket connection;
+            PrintWriter out;
+            BufferedReader in;
+
+            while(true){
+                String lineOut = "";
+                connection = server.accept();
+                in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String lineIn = in.readLine();
+
+                System.out.println("new message: " + lineIn);
+            }
 
         }catch (Exception e){
             e.printStackTrace();
